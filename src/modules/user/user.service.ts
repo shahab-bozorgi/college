204506b3d34337a45a062ab936032d3648dab846
Alireza Ghazanfari <alirezagnf99@gmail.com>
2024-08-08 01:06:31 +0330

    Add user service to sign up user.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,23 +1,41 @@
-import { HttpError } from "../../utilities/http-error";
+import { compare, hash } from "bcrypt";
+import { BadRequest, UnAuthorized } from "../../utilities/httpError";
+import { SignUpDto } from "./dto/create-user.dto";
+import { User } from "./model/user.model";
+import { IUserRepository } from "./user.repository";
 import { LoginUserDto } from "./dto/login-user.dto";
-import { UserRepository } from "./user.repository";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
 
 export class UserService {
-  constructor(private userRepo: UserRepository) {}
+  constructor(private repo: IUserRepository) {}
+
+  async create(dto: SignUpDto): Promise<User> {
+    if (await this.repo.findByUsername(dto.username)) {
+      throw new BadRequest("نام کاربری از قبل وجود داره!");
+    }
+
+    if (await this.repo.findByEmail(dto.email)) {
+      throw new BadRequest("آدرس ایمیل از قبل وجود داره!");
+    }
+
+    return await this.repo.create({
+      username: dto.username,
+      password: await hash(dto.password, 12),
+      email: dto.email,
+    });
+  }
 
   async login({ username, password }: LoginUserDto) {
-    const user = await this.userRepo.findByUsername(username);
+    const user = await this.repo.findByUsername(username);
 
-    if (!user || (user && (await bcrypt.compare(password, user.password))))
-      throw new HttpError(401, "username or password is invalid");
+    if (!user || (user && !(await compare(password, user.password))))
+      throw new UnAuthorized("نام کاربری یا رمز عبور اشتباهه!");
 
     const secretKey =
       typeof process.env.SECRET_KEY === "string"
         ? process.env.SECRET_KEY
-            : "secret-key";
-      
+        : "secret-key";
+
     const token = jwt.sign({ username }, secretKey, { expiresIn: "1h" });
 
     return token;

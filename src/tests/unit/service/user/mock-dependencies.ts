8cd3717f93695aa273800167e2517e4ee1a57a9d
Alireza Ghazanfari <alirezagnf99@gmail.com>
2024-08-08 15:09:42 +0330

    Add unit test for updating user inside repo.

--- src/tests/unit/service/user/mock-dependencies.ts
+++ src/tests/unit/service/user/mock-dependencies.ts
@@ -1,5 +1,9 @@
 import { v4 } from "uuid";
-import { CreateUser, User } from "../../../../modules/user/model/user.model";
+import {
+  CreateUser,
+  UpdateUser,
+  User,
+} from "../../../../modules/user/model/user.model";
 import { IUserRepository } from "../../../../modules/user/user.repository";
 import { UserId } from "../../../../modules/user/model/user-user-id";
 import { Username } from "../../../../modules/user/model/user-username";
@@ -7,14 +11,21 @@ import { Email } from "../../../../data/email";
 
 export class MockUserRepository implements IUserRepository {
   private users: User[] = [];
+
   async create(user: CreateUser): Promise<User> {
     const newUser = { ...user, id: v4() as UserId };
     this.users.push(newUser);
     return newUser;
   }
+
+  async update(userId: UserId, updatedUser: UpdateUser): Promise<boolean> {
+    throw Error("Implement This whenever needed");
+  }
+
   async findByUsername(username: Username): Promise<User | null> {
     return this.users.find((user) => user.username === username) ?? null;
   }
+
   async findByEmail(email: Email): Promise<User | null> {
     return this.users.find((user) => user.email === email) ?? null;
   }

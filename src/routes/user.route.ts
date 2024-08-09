66bc359b637f19c862acd70fb9ae1945eed0fccd
Alireza Ghazanfari <alirezagnf99@gmail.com>
2024-08-09 23:46:54 +0330

    Fix bug

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,6 @@
 import { Router } from "express";
-import { loginUserDto } from "../modules/user/dto/login-user.dto";
-import { handleExpress } from "../utilities/handle-express";
 import { UserService } from "../modules/user/user.service";
 import { toUsername, Username } from "../modules/user/model/user-username";
-import { UpdateUserSchema } from "../modules/user/dto/update-user.dto";
 
 export const makeUserRouter = (userService: UserService) => {
   const app = Router();
@@ -15,11 +12,6 @@ export const makeUserRouter = (userService: UserService) => {
     const username: Username = toUsername(req.params.username);
     const user = await userService.findByUsername(username);
     res.status(200).send(user);
-  });
-
-  app.patch("/edit-profile", (req, res) => {
-    const dto = UpdateUserSchema.parse(req.body.params);
-    handleExpress(res, () => userService.update(req.body, dto));
   });
 
   return app;

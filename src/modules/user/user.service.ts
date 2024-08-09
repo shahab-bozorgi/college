e611d26cf07a539952e500b9d82d552aa3b2f303
shahab-bozorgi <shahab.bozorgi007@gmail.com>
2024-08-09 13:07:57 +0330

    Add user-service to Edit profile & DTO

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,8 +4,10 @@ import { SignUpDto } from "./dto/create-user.dto";
 import { User } from "./model/user.model";
 import { IUserRepository } from "./user.repository";
 import { LoginUserDto } from "./dto/login-user.dto";
+import { EditProfileDto } from "./dto/edit-peofile-user.dto";
 import jwt from "jsonwebtoken";
 import { Username } from "./model/user-username";
+import { isUserId, UserId } from "./model/user-user-id";
 
 export class UserService {
   constructor(private repo: IUserRepository) {}
@@ -44,5 +46,36 @@ export class UserService {
     const token = jwt.sign({ username }, secretKey, { expiresIn: "1h" });
 
     return token;
+  }
+
+  async editProfile(username: Username, dto: EditProfileDto) {
+    const user = await this.repo.findByUsername(username);
+    if (!user) {
+      throw new BadRequest("کاربر وجود ندارد!");
+    }
+
+    if (dto.avatar_url !== undefined) user.avatar_url = dto.avatar_url;
+    if (dto.first_name !== undefined) user.first_name = dto.first_name;
+    if (dto.last_name !== undefined) user.last_name = dto.last_name;
+    if (dto.bio !== undefined) user.bio = dto.bio;
+
+    if (dto.email && dto.email !== user.email) {
+      if (await this.repo.findByEmail(dto.email)) {
+        throw new BadRequest("این ایمیل از قبل وجود دارد!");
+      }
+    }
+
+    if (dto.password && !(await compare(dto.password, user.password))) {
+      user.password = await hash(dto.password, 12);
+    }
+
+    // * Check in the next tasks *
+    if (dto.is_private == true){
+
+    }
+
+    const result = await this.repo.update(user.id, user);
+
+    return user;
   }
 }

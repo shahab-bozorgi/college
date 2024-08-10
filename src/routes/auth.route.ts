3b781b9501dfdbf936ce0eaf2232893b27b61bb9
Amir <amir29adib1234@gmail.com>
2024-08-10 20:16:00 +0330

    define user login and get user api

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -49,11 +49,43 @@ export const makeAuthRouter = (userService: UserService) => {
    *       '500':
    *         description: Internal server error
    */
-
   app.post("/sign-up", (req, res) => {
     const dto = SignUpSchema.parse(req.body);
     handleExpress(res, () => userService.create(dto));
   });
+
+  /**
+   * @swagger
+   * /v1/login:
+   *   post:
+   *     summary: Login user.
+   *     description: Post login data.
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               username:
+   *                 type: string
+   *                 description: Username of the user
+   *                 example: johndoe
+   *               password:
+   *                 type: string
+   *                 description: Password for the user
+   *                 example: P@ssw0rd
+   *             required:
+   *               - username
+   *               - password
+   *     responses:
+   *       '200':
+   *         description: A successful response
+   *       '400':
+   *         description: Bad request, possibly missing or incorrect data
+   *       '500':
+   *         description: Internal server error
+   */
   app.post("/login", (req, res) => {
     const dto = loginUserDto.parse(req.body);
     console.log("here");

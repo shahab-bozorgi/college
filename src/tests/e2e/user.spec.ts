61446a150def9b454b7db60657f818cba9147259
Amir <amir29adib1234@gmail.com>
2024-08-12 17:29:53 +0330

    done e2e test successfully

--- src/tests/e2e/user.spec.ts
+++ src/tests/e2e/user.spec.ts
@@ -1,19 +1,20 @@
 import request from "supertest";
-import { Express } from "express";
+import { Express, response } from "express";
 import { makeApp } from "../../api";
-import { TestDataSource } from "../../data-source";
+import { AppDataSource } from "../../data-source";
 import { seedUser } from "../seed";
+import { HttpError, UnAuthorized } from "../../utilities/http-error";
 
 describe("User test suite", () => {
   let app: Express;
   beforeAll(async () => {
-    const dataSource = await TestDataSource.initialize();
+    const dataSource = await AppDataSource.initialize();
     app = makeApp(dataSource);
     await seedUser();
   });
 
   afterAll(async () => {
-    await TestDataSource.destroy();
+    await AppDataSource.destroy();
   });
 
   describe("siginup", () => {
@@ -33,12 +34,17 @@ describe("User test suite", () => {
   describe("Login", () => {
     it("should user login if username and password is correct", async () => {
       await request(app)
-        .post("/login")
+        .post("/auth/login")
         .send({
           username: "ali",
-          password: "ali1234",
+          password: "Ali@1234",
         })
-        .expect(200);
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data).toHaveProperty("token");
+        });
     });
   });
 });
--- src/tests/seed.ts
+++ src/tests/seed.ts
@@ -1,21 +1,24 @@
 import { v4 } from "uuid";
-import { TestDataSource } from "../data-source";
+import { hash } from "bcrypt";
+import { AppDataSource } from "../data-source";
 import { UserEntity } from "../modules/user/entity/user.entity";
+import { UserId } from "../modules/user/model/user-user-id";
+import { Username } from "../modules/user/model/user-username";
+import { Password } from "../modules/user/model/user-password";
+import { CreateUser } from "../modules/user/model/user.model";
+import { Email } from "../data/email";
 
 export const seedUser = async () => {
-  const userRepo = TestDataSource.getRepository(UserEntity);
+  const userRepo = AppDataSource.getRepository(UserEntity);
 
   const count = await userRepo.count();
 
   if (count === 0) {
-    await userRepo.save([
-      {
-        id: v4(),
-        username: "ali",
-        password: "ali1234",
-        email: "alialavi@gmail.com",
-        avatar_url: "",
-      },
-    ]);
+    await userRepo.save({
+      id: v4() as UserId,
+      username: "ali" as Username,
+      password: await hash("Ali@1234", 12),
+      email: "alialavi@gmail.com",
+    });
   }
 };

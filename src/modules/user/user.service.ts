ffec6e2f7c5159072715871cdf5a776b158e1885
shahab-bozorgi <shahab.bozorgi007@gmail.com>
2024-08-13 13:22:31 +0330

    edit file name of user-profile

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -8,7 +8,7 @@ import { SignUpDto } from "./dto/create-user.dto";
 import { User } from "./model/user.model";
 import { IUserRepository } from "./user.repository";
 import { LoginUserDto } from "./dto/login-user.dto";
-import { EditProfileDto } from "./dto/edit-peofile-user.dto";
+import { EditProfileDto } from "./dto/edit-profile-user.dto";
 import jwt from "jsonwebtoken";
 import { Username } from "./model/user-username";
 import { isEmail } from "../../data/email";

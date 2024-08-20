56e1c1ff77309585505aabbc1bec433ec1dbd02c
Alireza Ghazanfari <alirezagnf99@gmail.com>
2024-08-20 17:02:49 +0330

    Fix unit test mock dependency.

--- src/tests/unit/service/user/mock-dependencies.ts
+++ src/tests/unit/service/user/mock-dependencies.ts
@@ -10,15 +10,16 @@ import { Username } from "../../../../modules/user/model/user-username";
 import { Email } from "../../../../data/email";
 
 export class MockUserRepository implements IUserRepository {
-  update(id: UserId, fields: UpdateUser): Promise<User | null> {
-    throw new Error("Method not implemented.");
-  }
   private users: User[] = [];
 
   async create(user: CreateUser): Promise<User> {
     const newUser = { ...user, id: v4() as UserId };
     this.users.push(newUser);
     return newUser;
+  }
+
+  update(id: UserId, fields: UpdateUser): Promise<User | null> {
+    throw new Error("Method not implemented.");
   }
 
   async findById(id: UserId): Promise<User | null> {
@@ -31,5 +32,9 @@ export class MockUserRepository implements IUserRepository {
 
   async findByEmail(email: Email): Promise<User | null> {
     return this.users.find((user) => user.email === email) ?? null;
+  }
+
+  whereUsernameIn(usernames: Username[]): Promise<User[]> {
+    throw new Error("Method not implemented.");
   }
 }

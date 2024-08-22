85b8641110327ec643535089f649c9fe89b8e461
Amir <amir29adib1234@gmail.com>
2024-08-22 10:53:09 +0330

    correct user info for comments

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -118,7 +118,9 @@ export const makePostRouter = (
 
     const comment2 = {
       id: v4(),
-      userId: 'd9cccca3-9dc5-4479-bf40-bc8fe146b026',
+      username: "arefe",
+      firstname: "Arefe",
+      lastname: "Alavi",
       parentId: comment1Id,
       postId: req.params.postId,
       description: description2,

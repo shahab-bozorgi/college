6efea40340e765da18e10795a399dc231c5fa55e
Amir <amir29adib1234@gmail.com>
2024-08-23 20:09:38 +0330

    improve postId model

--- src/modules/post/model/post-id.ts
+++ src/modules/post/model/post-id.ts
@@ -1,3 +1,9 @@
+import { z } from "zod";
 import { Brand } from "../../../utilities/brand";
+import { isUUID } from "../../../data/uuid";
 
 export type PostId = Brand<string, "PostId">;
+
+export const isPostId = (value: string): value is PostId => isUUID(value);
+
+export const zodPostId = z.string().refine(isPostId);

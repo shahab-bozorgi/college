ce7e82c175aea1545697630f60019605b319b771
Amir <amir29adib1234@gmail.com>
2024-09-19 21:41:10 +0330

    edit comment orphans for replies data

--- src/modules/post/comment/comment.service.ts
+++ src/modules/post/comment/comment.service.ts
@@ -117,7 +117,7 @@ export class CommentService {
               })) !== null
                 ? true
                 : false,
-            replies: [],
+            replies: this.buildCommentTree(orphan, orphans),
           });
         }
       }
